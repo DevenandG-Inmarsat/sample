ded8f9874e26b17236432fbb10f38a52782e2437
DevenandGInmarsat <devenand.g@WIN-HN4QLB8N7QU.aws.inmarsat>
2017-04-12 18:20:42 +0530

    my first commit

---
+++ src/main/java/com/test/inmarsat/samplerest/SampleRoute.java
@@ -0,0 +1,27 @@
+package com.test.inmarsat.samplerest;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRoute extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		restConfiguration().component("restlet").port(9091);
+		rest("/expose")
+		.get("/rest").produces("text/plain")
+		.to("direct:samplerest");
+		
+		
+		//from("cxfrs:http://localhost:8081?resourceClasses=com.test.inmarsat.samplerest.SampleService&bindingStyle=SimpleConsumer")
+		from("direct:samplerest")
+		.log("Inside the Rest sample Project Setting the body")
+	    .setBody().constant("Hello from the Rest project") 
+		.log("the body :: ${body}");
+	    
+	}
+
+	
+	
+}
---
+++ src/main/java/com/test/inmarsat/samplerest/SampleService.java
@@ -0,0 +1,13 @@
+package com.test.inmarsat.samplerest;
+
+/*import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/sample")*/
+public interface SampleService {
+
+	
+	/*@Path("/rest")
+	@GET*/
+	public String sampleRestService(); 
+}
