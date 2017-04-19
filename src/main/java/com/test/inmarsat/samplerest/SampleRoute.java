3d2455ac4e25a73f2d66cdef5639d1c1faa1403e
DevenandGInmarsat <devenand.g@WIN-HN4QLB8N7QU.aws.inmarsat>
2017-04-19 19:42:08 +0530

    port changed

--- src/main/java/com/test/inmarsat/samplerest/SampleRoute.java
+++ src/main/java/com/test/inmarsat/samplerest/SampleRoute.java
@@ -8,7 +8,7 @@ public class SampleRoute extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		restConfiguration().component("restlet").port(8080);
+		restConfiguration().component("restlet").port(9091);
 		rest("/expose")
 		.get("/rest").produces("text/plain")
 		.to("direct:samplerest");
